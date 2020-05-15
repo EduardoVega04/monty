276c6fc546b9a64607f0482caba4372ec1e3eae9
EduardoVega04 <eduarvega04@hotmail.com>
2020-05-15 02:57:47 +0000

    Added fclose

---
+++ bytecodes/000.m
@@ -0,0 +1,9 @@
+
+
+push hola
+push 3
+pall
+push  2                         2
+push 3
+pall
+
--- main.c
+++ main.c
@@ -18,6 +18,7 @@ int main(int argc, char *argv[])
 	}
 
 	read_file(fp);
+	fclose(fp);
 
 	return (0);
 }
